c1634375b063731da4a594885ad3057c35aacedc
etchichury <joaoetchichury@hotmail.com>
2021-01-25 23:48:24 -0300

    Refactor to use more composition in InfoColumn

--- src/components/BasicInfoCard/index.tsx
+++ src/components/BasicInfoCard/index.tsx
@@ -1,14 +1,23 @@
 import React from "react";
 import { FaMoneyBill } from "react-icons/fa";
 import { MdArrowDownward, MdArrowUpward } from "react-icons/md";
-import { InfoColumn } from "../InfoColumn";
+import { InfoColumn, InfoHeader, InfoValue } from "../InfoColumn";
 
 export const BasicInfoCard = () => {
   return (
-    <div className="border border-black-300 bg-white ml-28 mr-12 mt-6 rounded-lg p-4 flex flex-row justify-between">
-      <InfoColumn Icon={FaMoneyBill} title="Total balance" />
-      <InfoColumn Icon={MdArrowDownward} title="Income" />
-      <InfoColumn Icon={MdArrowUpward} title="Outgoing" />
+    <div className="border border-black-300 bg-white ml-28 mr-12 mt-6 rounded-lg p-10 flex flex-row justify-between">
+      <InfoColumn>
+        <InfoHeader Icon={FaMoneyBill} title="Total balance" />
+        <InfoValue valueStyle="text-2xl font-bold" value={17928.63} />
+      </InfoColumn>
+      <InfoColumn>
+        <InfoHeader Icon={MdArrowDownward} title="Income" />
+        <InfoValue valueStyle="text-xl" value={5229.13} />
+      </InfoColumn>
+      <InfoColumn>
+        <InfoHeader Icon={MdArrowUpward} title="Outgoing" />
+        <InfoValue valueStyle="text-xl" value={2348.75} />
+      </InfoColumn>
     </div>
   );
 };
--- src/components/InfoColumn/index.tsx
+++ src/components/InfoColumn/index.tsx
@@ -1,17 +1,33 @@
 import React from "react";
-import { IconType } from "react-icons";
+import { IconType } from "react-icons/lib";
 
 type InfoProps = {
+  children: React.ReactNode;
+};
+
+type HeaderProps = {
   Icon: IconType;
   title: string;
 };
 
-export const InfoColumn = ({ Icon, title }: InfoProps) => {
+type ValueProps = {
+  valueStyle: string;
+  value: number;
+};
+
+export const InfoHeader = ({ Icon, title }: HeaderProps) => {
   return (
-    <div className="flex flex-column">
-      <div className="flex flex-row text-2xl font-semibold">
-        <Icon className="self-center mr-4" size={24} /> <h1>{title}</h1>
-      </div>
+    <div className="flex flex-row mb-2">
+      <Icon className="self-center mr-2" size={32} />{" "}
+      <div className="text-xl font-medium">{title}</div>
     </div>
   );
+};
+
+export const InfoValue = ({ valueStyle, value }: ValueProps) => {
+  return <div className={valueStyle}>R$ {value}</div>;
+};
+
+export const InfoColumn = ({ children }: InfoProps) => {
+  return <div className="flex flex-col">{children}</div>;
 };
