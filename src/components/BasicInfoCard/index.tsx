246a1ce55c669ee5db753674e6134e6e73089e19
etchichury <joaoetchichury@hotmail.com>
2021-01-27 23:18:28 -0300

    Add string formatting to values

--- src/components/BasicInfoCard/index.tsx
+++ src/components/BasicInfoCard/index.tsx
@@ -8,15 +8,24 @@ export const BasicInfoCard = () => {
     <div className="border border-black-300 bg-white ml-28 mr-12 mt-6 rounded-lg p-10 flex flex-row justify-between">
       <InfoColumn>
         <InfoHeader Icon={FaMoneyBill} title="Total balance" />
-        <InfoValue valueStyle="text-2xl font-bold" value={17928.63} />
+        <InfoValue
+          valueStyle="text-3xl font-bold"
+          value={(17928.63).toLocaleString("en")}
+        />
       </InfoColumn>
       <InfoColumn>
         <InfoHeader Icon={MdArrowDownward} title="Income" />
-        <InfoValue valueStyle="text-xl" value={5229.13} />
+        <InfoValue
+          valueStyle="text-2xl font-semibold"
+          value={(5229.13).toLocaleString("en")}
+        />
       </InfoColumn>
       <InfoColumn>
         <InfoHeader Icon={MdArrowUpward} title="Outgoing" />
-        <InfoValue valueStyle="text-xl" value={2348.75} />
+        <InfoValue
+          valueStyle="text-2xl font-semibold"
+          value={(2348.75).toLocaleString("en")}
+        />
       </InfoColumn>
     </div>
   );
