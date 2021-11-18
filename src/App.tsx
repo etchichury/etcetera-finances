f2f336748d20d981612d545c0f5b2a426a48675e
etchichury <joaoetchichury@hotmail.com>
2021-11-18 00:02:16 -0300

    Render Account page and some text in the nested ones

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,22 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Account from "src/pages/account";
 import GlobalStyle from "src/GlobalStyles";
 
 function App() {
   return (
     <>
       <GlobalStyle />
-    <BrowserRouter>
-      <Routes>
-        <Route path="login" />
-        <Route path="register" />
-        <Route path="account">
-          <Route path="expenses" />
-          <Route path="budget" />
-        </Route>
-      </Routes>
-    </BrowserRouter>
+      <BrowserRouter>
+        <Routes>
+          <Route path="login" />
+          <Route path="register" />
+          <Route path="account" element={<Account />}>
+            <Route path="expenses" element={<div>General Kennobi!</div>} />
+            <Route path="budget" element={<div>Nooooo!</div>} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </>
   );
 }
 
