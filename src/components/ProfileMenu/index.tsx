dcc095ab843c90b33531b72af0595d0927f3df67
etchichury <joaoetchichury@hotmail.com>
2021-12-12 12:39:24 -0300

    Create profile menu component

---
+++ src/components/ProfileMenu/index.tsx
@@ -0,0 +1,40 @@
+import { ChevronCompactDown, ChevronCompactUp } from "@styled-icons/bootstrap";
+import { useState } from "react";
+import {
+  ProfileContainer,
+  ProfileSummaryContainer,
+  ProfileImage,
+  ProfileOptionsContainer,
+} from "./styles";
+
+const ProfileMenu = ({
+  userName,
+  userImg,
+}: {
+  userName: string;
+  userImg: string;
+}) => {
+  const [showMenu, setShowMenu] = useState(false);
+
+  return (
+    <ProfileContainer>
+      <ProfileSummaryContainer onClick={() => setShowMenu(!showMenu)}>
+        <ProfileImage src={userImg} alt="user" width={45} height={45} />
+        <h4>{userName}</h4>
+        {showMenu ? (
+          <ChevronCompactUp size={28} />
+        ) : (
+          <ChevronCompactDown size={28} />
+        )}
+      </ProfileSummaryContainer>
+      {showMenu ? (
+        <ProfileOptionsContainer>
+          <hr />
+          <h5>Logout</h5>
+        </ProfileOptionsContainer>
+      ) : null}
+    </ProfileContainer>
+  );
+};
+
+export default ProfileMenu;
---
+++ src/components/ProfileMenu/styles.ts
@@ -0,0 +1,33 @@
+import styled from "styled-components";
+
+export const ProfileContainer = styled.div`
+  box-shadow: rgba(0, 0, 0, 0.16) 0px 1px 4px;
+  border-radius: 5px;
+  margin-top: 25px;
+`;
+
+export const ProfileSummaryContainer = styled.div`
+  display: grid;
+  grid-template-columns: 1fr 3fr 0.7fr;
+  align-items: center;
+  cursor: pointer;
+  padding: 10px 0px;
+  h4 {
+    overflow: hidden;
+    white-space: nowrap;
+    text-overflow: ellipsis;
+  }
+`;
+
+export const ProfileOptionsContainer = styled.div`
+  hr {
+    margin: 5px 10px;
+    border: 1px solid #f0f0f0;
+  }
+`;
+
+export const ProfileImage = styled.img`
+  border: solid 1px #97ead2;
+  border-radius: 50%;
+  justify-self: center;
+`;
