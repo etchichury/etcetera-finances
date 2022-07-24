cdf1f5a4734583a8fe39c6198cf852659f05e076
etchichury <joaoetchichury@hotmail.com>
2022-07-24 02:54:04 -0300

    Add dynamic color to button component

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -5,32 +5,46 @@ export enum ButtonStyle {
   Secondary,
 }
 
+export enum ButtonColor {
+  Blue,
+  Green,
+}
+
 type ButtonProps = {
   style: ButtonStyle;
+  color: ButtonColor;
   width: number;
   height: number;
   onClick: () => void;
   disabled?: boolean;
 };
 
-// TODO: dynamic color
 const Button: React.FC<ButtonProps> = ({
   style,
+  color,
   width,
   height,
   onClick,
   disabled,
   children,
 }) => {
+  const colors = {
+    0: "#002E5C",
+    1: "#8CC7A1",
+  };
+
+  const primaryButtonTextColor =
+    color === ButtonColor.Blue ? "white" : "#002E5C";
+
   const styles = {
     0: {
-      background: disabled ? "#d0d0d0" : "#002E5C",
+      background: disabled ? "#d0d0d0" : colors[color],
       border: "inherit",
-      color: disabled ? "#4d4d4d" : "white",
+      color: disabled ? "#4d4d4d" : primaryButtonTextColor,
     },
     1: {
       background: "transparent",
-      border: "solid 4px #002E5C",
+      border: `solid 4px ${disabled ? "#d0d0d0" : colors[color]}`,
       color: "#002E5C",
     },
   };
--- src/pages/Budget/index.tsx
+++ src/pages/Budget/index.tsx
@@ -1,10 +1,22 @@
-import Button, { ButtonStyle } from "src/components/Button";
+import Button, { ButtonStyle, ButtonColor } from "src/components/Button";
 
 const Budget = () => {
   return (
     <div style={{ padding: "400px" }}>
       <Button
         style={ButtonStyle.Primary}
+        color={ButtonColor.Blue}
+        onClick={() => {
+          console.log("Accept");
+        }}
+        width={150}
+        height={50}
+      >
+        Accept
+      </Button>
+      <Button
+        style={ButtonStyle.Primary}
+        color={ButtonColor.Green}
         onClick={() => {
           console.log("Accept");
         }}
@@ -15,6 +27,7 @@ const Budget = () => {
       </Button>
       <Button
         style={ButtonStyle.Secondary}
+        color={ButtonColor.Blue}
         onClick={() => {
           console.log("Cancel");
         }}
@@ -25,6 +38,7 @@ const Budget = () => {
       </Button>
       <Button
         style={ButtonStyle.Primary}
+        color={ButtonColor.Blue}
         onClick={() => {
           console.log("Accept");
         }}
@@ -33,6 +47,18 @@ const Budget = () => {
         disabled
       >
         Accept
+      </Button>
+      <Button
+        style={ButtonStyle.Secondary}
+        color={ButtonColor.Blue}
+        onClick={() => {
+          console.log("Accept");
+        }}
+        width={150}
+        height={50}
+        disabled
+      >
+        Cancel
       </Button>
     </div>
   );
