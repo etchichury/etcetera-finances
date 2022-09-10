f58b073b6537db70b625d7753958539fc956637a
etchichury <joaoetchichury@hotmail.com>
2022-09-10 13:02:32 -0300

    Remove width and height props from Button tests

--- src/tests/Button.test.tsx
+++ src/tests/Button.test.tsx
@@ -1,35 +1,31 @@
-import Button, { ButtonColor, ButtonStyle } from "src/components/Button";
-import { render, screen } from "@testing-library/react";
+import Button, { ButtonColor, ButtonStyle } from "src/components/Button"
+import { render, screen } from "@testing-library/react"
 
 test("Button component rendering desired content", () => {
   render(
     <Button
       style={ButtonStyle.Primary}
       color={ButtonColor.Blue}
-      width={100}
-      height={100}
       onClick={jest.fn()}
     >
       Accept
     </Button>
-  );
+  )
 
-  expect(screen.getByRole("button")).toHaveTextContent(/accept/i);
-});
+  expect(screen.getByRole("button")).toHaveTextContent(/accept/i)
+})
 
 test("Disable button state", () => {
   render(
     <Button
       style={ButtonStyle.Primary}
       color={ButtonColor.Blue}
-      width={100}
-      height={100}
       onClick={jest.fn()}
       disabled
     >
       Accetpt
     </Button>
-  );
+  )
 
-  expect(screen.getByRole("button")).toBeDisabled();
-});
+  expect(screen.getByRole("button")).toBeDisabled()
+})
