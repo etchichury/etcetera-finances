1f6800b9c455a8b4444be966ea4737bd71eaaaa9
etchichury <joaoetchichury@hotmail.com>
2022-11-12 18:35:49 -0300

    Upgrade to React 18 root render

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,10 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import App from "src/App";
+import React from "react"
+import { createRoot } from "react-dom/client"
+import App from "src/App"
 
-// FIXME: ReactDOM.render is not supported
-ReactDOM.render(
+const root = createRoot(document.getElementById("root")!)
+root.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-  document.getElementById("root")
-);
+  </React.StrictMode>
+)
