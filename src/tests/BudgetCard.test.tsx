ad3429c73aea0e87a3c84ff09c1dbeb6c8f6ecfc
etchichury <joaoetchichury@hotmail.com>
2022-11-15 00:52:09 -0300

    Add contextual menu rendering test

--- src/tests/BudgetCard.test.tsx
+++ src/tests/BudgetCard.test.tsx
@@ -31,3 +31,26 @@ test("BudgetCard renders desired content when expanded", async () => {
   expect(screen.getByRole("button", { name: /clear all/i })).toBeInTheDocument()
   expect(screen.getByRole("button", { name: /add entry/i }))
 })
+
+test("renders contextual menu when right clicking table cell", async () => {
+  const user = userEvent.setup()
+  render(
+    <BudgetCard
+      budgetName='Groceries'
+      budgetLimit={800}
+      currentProgress={300}
+      expenses={[
+        { id: 1, description: "Almoço 06/08", value: 123.34 },
+        { id: 2, description: "Mercado 17/12", value: 800.34 },
+      ]}
+    />
+  )
+
+  await user.click(screen.getByRole("button", { name: /expand content/i }))
+
+  const expenseCell = screen.getByRole("cell", { name: "Almoço 06/08" })
+  await user.pointer({ target: expenseCell, keys: "[MouseRight]" })
+
+  expect(screen.getByRole("menuitem", { name: /edit/i })).toBeInTheDocument()
+  expect(screen.getByRole("menuitem", { name: /delete/i })).toBeInTheDocument()
+})
