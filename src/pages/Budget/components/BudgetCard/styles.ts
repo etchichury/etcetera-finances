d17eee6b1d350d88b127237b8882636bce2705dd
etchichury <joaoetchichury@hotmail.com>
2023-06-04 18:46:37 -0300

    Change hr background color

--- src/pages/Budget/components/BudgetCard/styles.ts
+++ src/pages/Budget/components/BudgetCard/styles.ts
@@ -12,12 +12,7 @@ export const BudgetCardContainer = styled.article`
   > hr {
     border: 0;
     height: 1px;
-    background-image: linear-gradient(
-      to right,
-      rgba(0, 0, 0, 0.2),
-      rgba(0, 0, 0, 0.75),
-      rgba(0, 0, 0, 0.2)
-    );
+    background: rgba(0, 0, 0, 0.2);
   }
 `
 
